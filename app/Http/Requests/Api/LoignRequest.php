a72f567337947e1537e14aac08817670de3d0c1e
Edris Mazhari <mazhari.edris@gmail.com>
2024-11-14 11:05:11 +0330

    add todo with auth

--- app/Http/Requests/Api/LoignRequest.php
+++ app/Http/Requests/Api/LoignRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\Api;
 
 use App\BaseContext\ApiRequest;
-use Illuminate\Foundation\Http\FormRequest;
 
 class LoignRequest extends ApiRequest
 {
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,19 @@
 
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\TodoController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 Route::post('login',LoginController::class);
 
 
-Route::middleware('auth:sanctum')->group(function(){
-    Route::apiResource('todos',TodoController::class);
-});
+Route::middleware('auth:sanctum')->apiResource('notes',TodoController::class)->only(['index','store','show']);
+
+
+// Route::middleware('auth:sanctum')->group(function(){
+//     Route::apiResource('todos',TodoController::class);
+// });
